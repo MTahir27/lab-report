37dd3037e12f901d0edbaad6375838f0d8376ec0
Muhammad Tahir <m.tahiridrees27@gmail.com>
2022-09-21 22:47:56 +0500

    Live DB Added

--- connection.php
+++ connection.php
@@ -1,11 +1,17 @@
 <?php
 
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "labreport";
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+	// $dbname = "labreport";
+	// $link = mysqli_connect("localhost", "root", "", "labreport");
 	
-	$link = mysqli_connect("localhost", "root", "", "labreport");
+	$servername = "sql212.epizy.com";
+	$username = "epiz_32641152";
+	$password = "I9kV29pQ3BiG";
+	$dbname = "epiz_32641152_labreport";
+	
+	$link = mysqli_connect($servername, $username, $password, $dbname);
 	
 	if($link)
     {
