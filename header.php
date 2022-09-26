9a69a8381d85bc662f51f9c5d630183991bbfa9c
Muhammad Tahir <m.tahiridrees27@gmail.com>
2022-09-26 11:43:19 +0500

    Updating

--- header.php
+++ header.php
@@ -1,4 +1,14 @@
 <?php
+
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+    header("location: login.php");
+    exit;
+}
+
+
+?>
+<?php
 include 'connection.php';
 ?>
 
